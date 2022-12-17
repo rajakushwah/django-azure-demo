be53c9cc7ff3c5f5dd907ca4dd579574c0815d65
rajakushwah <39225004+rajakushwah@users.noreply.github.com>
2022-12-17 23:20:06 +0530

    Update urls.py

--- food/urls.py
+++ food/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
-from django.urls import path
-from food import views
-
+from . import views
+from django.urls import path,include
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',views.home,name='home' ),
+    # path('observation/',views.observation,name='observation' ),
+    path('encounter/',views.encounter,name='encounter' ),
+    path('jsonviewPatient/<id>/',views.jsonviewPatient, name='jsonviewPatient' ),
+    # path('jsonviewObservation/<id>/', views.jsonviewObservation, name='jsonviewObservation'),
+    # path('jsonviewEncounter/<id>/', views.jsonviewEncounter, name='jsonviewEncounter'),
+    path('url/', views.url, name='url'),
+
 ]
